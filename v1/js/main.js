a896e2591a3543b73d0c862a15f9b52f8d142016
KevinMokoro <klosvoke1@gmail.com>
2022-09-23 10:59:44 +0300

    Portfolio

---
+++ v1/js/main.js
@@ -0,0 +1,20 @@
+/*Smooth scrolling*/
+$('.nav-item a').on('click',function (event){
+    if(this.hash !== ''){
+        event.preventDefault();
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top
+            },
+            800,
+            function(){
+                window.location.hash = hash;
+            }
+        );
+    }
+})
+
+
+      
